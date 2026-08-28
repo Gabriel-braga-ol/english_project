a996aae40f7bd477515e1de72176cacdd14e7160
Gabriel Braga <bielbraga37@outlook.com>
2026-08-27 23:07:47 -0300

    test: expande os casos de extração de palavras

--- test_text_utils.py
+++ test_text_utils.py
@@ -22,9 +22,12 @@ def test_normalize_word(input_word, expected_word):
     [
         ("February 4, 2004", ["february"]),
         ("123 456", []),
+        ("Hello, Python!", ['hello', 'python']),
+        ("I don't know.", ["i", "don't", "know"]),
+        ("I use Python3 and HTML5", ["i", "use", "python3", "and", "html5"]),
     ]
 )
-def test_extract_words_ignores_numbers(text, expected):
+def test_normalize_words_and_ignores_numbers(text, expected):
     assert extract_words(text) == expected
 
 
